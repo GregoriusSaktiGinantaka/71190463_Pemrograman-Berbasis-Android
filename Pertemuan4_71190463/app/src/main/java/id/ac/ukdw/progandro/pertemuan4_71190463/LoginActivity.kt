524724fca188baada1c7a76ac68ec105720a9049
GregoriusSaktiGinantaka <89846301+GregoriusSaktiGinantaka@users.noreply.github.com>
2021-09-27 21:46:22 +0700

    Add files via upload

---
+++ Pertemuan4_71190463/app/src/main/java/id/ac/ukdw/progandro/pertemuan4_71190463/LoginActivity.kt
@@ -0,0 +1,11 @@
+package id.ac.ukdw.progandro.pertemuan4_71190463
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
